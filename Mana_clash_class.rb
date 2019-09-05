18ee9175c71cd0a6b1d42106bfeaf23a010dd45f
luke <l.j.dwsn@gmail.com>
2019-09-05 15:10:45 +1000

    Rubocop updates and beginning on 'remove' feature

--- Mana_clash_class.rb
+++ Mana_clash_class.rb
@@ -1,239 +1,158 @@
+# frozen_string_literal: true
+
 require 'csv'
 require 'pry'
 require 'artii'
 require 'colorize'
-require "tty-prompt"
+require 'tty-prompt'
 
 cards = []
 
 def subtext(type)
+  if type == 'spell'
 
-    if type == "spell"
+    puts 'simply spellbinding...'
 
-        puts "simply spellbinding..."
+    puts `clear`
 
-        puts `clear` 
+  elsif type == 'creature'
 
-    elsif type == "creature"
+    puts 'Ah.. into the creature comforts I see'
 
-        puts "Ah.. into the creature comforts I see"
+    puts `clear`
 
-        puts `clear`
+  else
 
-    else
+    puts "I don't understand"
 
-        puts "I don't understand"
-
-    end
-
+  end
 end
-
 
 def getAColor
+  puts 'Is your card blue, red, white, black or green?'
 
-    puts "Is your card blue, red, white, black or green?"
+  colours = %w[red blue green white black]
 
-    colours = ['red', 'blue', 'green', 'white', 'black']
+  colour = gets.chomp.downcase
 
-    colour = gets.chomp.downcase
+  if colours.include?(colour)
 
+    puts "Your card is #{colour}"
 
+    puts `clear`
 
-    if colours.include?(colour)
+    return colour
 
-        puts "Your card is #{colour}"
+  else
 
-        puts `clear`
+    puts 'I do not understand'
 
-        return colour
+    getAColor
 
-    else
-
-        puts "I do not understand"
-
-        getAColor
-
-    end
-
-
-
+  end
 end
-
-
 
 def subtype(type)
+  if type == 'spell'
 
-    if type == "spell"
+    puts "What attribute would you like your spell to have? \n Deal damage \n Counter a spell \n Mend damage"
 
-         puts "What attribute would you like your spell to have? \n Deal damage \n Counter a spell \n Mend damage" 
+    subtype_spell = gets.chomp
 
-        subtype_spell = gets.chomp  
+    spell_ability(subtype_spell)
 
-        spell_ability(subtype_spell)
+    subtype_spell
 
-        return subtype_spell
+  elsif type == 'creature'
 
-    elsif type == "creature"
+    puts "What ability would you like your creature to have? \n Double Strike \n Lifegain \n Speed "
 
-        puts "What ability would you like your creature to have? \n Double Strike \n Lifegain \n Speed "
+    subtype_creature = gets.chomp
 
-        subtype_creature = gets.chomp
+    creature_ability(subtype_creature)
 
-        creature_ability(subtype_creature)
+    subtype_creature
 
-        return subtype_creature
-
-    end     
-
+  end
 end
-
-
 
 def spell_ability(subtype)
+  puts 'You like power' if subtype == 'Deal damage'
 
-    if subtype == "Deal damage"
+  puts 'You like to protect' if subtype == 'Counter a spell'
 
-        puts "You like power"
-
-    end
-
-
-
-    if subtype == "Counter a spell"
-
-        puts "You like to protect"
-
-    end
-
-
-
-    if subtype == "Mend damage"
-
-
-
-        puts "You like to heal"
-
-    end
-
+  puts 'You like to heal' if subtype == 'Mend damage'
 end
-
-
 
 def creature_ability(subtype)
+  puts 'You covert strength' if subtype == 'Double strike'
 
-    if subtype == "Double strike"
+  puts 'you favour survivability' if subtype == 'Lifegain'
 
-        puts "You covert strength"
-
-    end
-
-
-
-    if subtype == "Lifegain"
-
-        puts "you favour survivability"
-
-    end
-
-
-
-    if subtype == "Speed"
-
-        puts "those that strike first, win"
-
-    end
-
+  puts 'those that strike first, win' if subtype == 'Speed'
 end
-
-
 
 def buildACard
+  puts `clear`
 
-    puts `clear`
+  puts 'How much does your card cost in mana'
 
-    puts "How much does your card cost in mana"
+  cost = gets.chomp
 
-    cost = gets.chomp
+  if cost.to_i.to_s != cost
 
+    puts 'That is not a number'
 
+    buildACard
 
-    if cost.to_i.to_s != cost
+  end
 
-        puts "That is not a number"
+  puts `clear`
 
-        buildACard    
+  puts 'Is your card a spell or a creature'
 
-    end
+  type = gets.chomp
 
-    
+  # If type is x, use subtext method
 
-    
+  subtext(type)
 
-    
+  colour = getAColor
 
+  puts 'What is the name of your creation'
 
+  name = gets.chomp
 
+  puts `clear`
 
+  ability = subtype(type)
 
-    puts `clear`
+  puts `clear`
 
-    puts "Is your card a spell or a creature"  
+  puts 'Would you like to include a description?'
 
-    type = gets.chomp
+  flavour = gets.chomp
 
-    # If type is x, use subtext method
+  puts `clear`
 
-    subtext(type)
+  puts "Congrats! #{name} is born."
 
-    colour = getAColor
+  CSV.open('card_list.csv', 'a+', headers: true) do |csv|
+    csv << [cost, name, colour, type, ability, flavour]
+  end
 
-
-
-    puts "What is the name of your creation" 
-
-    name = gets.chomp 
-
-    puts `clear`
-
-
-
-    ability = subtype(type)
-
-    puts `clear`
-
-    puts "Would you like to include a description?" 
-
-    flavour = gets.chomp 
-
-    puts `clear`
-
-    puts "Congrats! #{name} is born."
-
-
-
-    CSV.open("card_list.csv", "a+", {headers: true}) do |csv|
-
-        csv << [cost, name, colour, type, ability , flavour]
-
-    end
-
-    return {cost: cost, name: name, colour: colour, type: type, ability: ability, flavour: flavour}
-
+  { cost: cost, name: name, colour: colour, type: type, ability: ability, flavour: flavour }
 end
 
-    #edit(cost, type, colour, name, flavour)
-
+# edit(cost, type, colour, name, flavour)
 
 def showHeader
+  puts `artii 'Mana Clash'`.colorize(color: :blue)
 
-    puts `artii 'Mana Clash'`.colorize(:color => :blue)      
+  puts `artii 'You are'`.colorize(color: :red)
 
-    puts `artii 'You are'`.colorize(:color => :red)  
-
-    puts `artii 'the creator'`.colorize(:color => :green)
-
+  puts `artii 'the creator'`.colorize(color: :green)
 end
-
 
 # Above this line is all my methods
 
@@ -241,78 +160,80 @@ end
 
 showHeader
 
-
-
 loop do
+  puts 'Welcome to Manaclash card creator and editor! What would you like to do:'
+  puts '1. Create a new card'
+  puts '2. Edit a card'
+  puts '3. Remove a card'
+  puts '4. Exit'
 
+  print 'Enter Option Number: '
 
+  input = gets.chomp
 
-            puts "Welcome to Manaclash card creator and editor! What would you like to do:"
-            puts "1. Create a new card"
-            puts "2. Edit a card"
-            puts "3. Remove a card"
-            puts "4. Exit"
+  if input == '1'
 
-            print "Enter Option Number: "
+    card = buildACard
 
+    puts card
 
+    cards << card
 
-            input = gets.chomp
+  elsif input == '2'
 
-        if input == "1"
+    prompt = TTY::Prompt.new
 
-             card = buildACard
+    card_list = CSV.read('Card_list.csv')
+    card_list.shift
 
-             puts card
+    card_list.each do |card|
+      p card
+    end
 
-             cards << card
+    puts "What card would you like to edit (name? \n
 
-        elsif input == "2"
+                                                 "
+    input = gets.chomp
 
-            prompt = TTY::Prompt.new
+    found_card = card_list.find { |card| card.include?(input) }
+    puts found_card
 
-            card_list = CSV.read("Card_list.csv")
-            card_list.shift
+    # binding.pry
 
-            card_list.each do |card|
-                p card        
-            end
+    puts "how'd you element would you like to change"
+    new_input = gets.chomp
 
-           puts   "What card would you like to edit (name? \n
+    card_list[card_list.index(found_card)][1] = new_input
+    p card_list
+    CSV.open('Card_list.csv', 'w') do |csv|
+      card_list.each do |card|
+        csv << card
+      end
+    end
 
-                                                        "
-            input = gets.chomp
-            
-            found_card = card_list.find{|card| card.include?(input)}
-            puts found_card
+  elsif input == '3'
 
-            #binding.pry
+    puts 'This WILL remove your cards'
 
-            puts "how'd you element would you like to change"
-            new_input = gets.chomp
+    card_list = CSV.read('Card_list.csv')
+    card_list.shift
 
-           card_list[card_list.index(found_card)][1] = new_input
-            p card_list
-            CSV.open("Card_list.csv", "w") do |csv|
-                card_list.each do |card|
-                    csv << card
-                end
-            end
+    card_list.each do |card|
+      p card
+    end
 
-        elsif input == "3"
+    puts "What card would you like to remove?\n
 
-        puts "This WILL remove your cards"
+                                                 "
+    input = gets.chomp
 
-        elsif input ==  "4"
+  elsif input == '4'
 
-            break
+    break
 
-        else 
+  else
 
-          puts "anything"
+    puts 'anything'
 
-
-        end
-
+  end
 end
-
