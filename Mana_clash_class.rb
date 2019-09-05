9776389a46e8706264ce462f73cf36687b05230c
luke <l.j.dwsn@gmail.com>
2019-09-05 13:06:37 +1000

    Adding edit feature

--- Mana_clash_class.rb
+++ Mana_clash_class.rb
@@ -1,165 +1,330 @@
 require 'csv'
+
 require 'pry'
 
+
+
 require 'artii'
+
 require 'colorize'
+
 require "tty-prompt"
+
+
+
 
 
 cards = []
 
 
+
+
+
 def subtext(type)
+
     if type == "spell"
+
         puts "simply spellbinding..."
+
         puts `clear` 
+
     elsif type == "creature"
+
         puts "Ah.. into the creature comforts I see"
+
         puts `clear`
+
     else
+
         puts "I don't understand"
+
     end
+
 end
+
+
 
 def getAColor
+
     puts "Is your card blue, red, white, black or green?"
+
     colours = ['red', 'blue', 'green', 'white', 'black']
+
     colour = gets.chomp.downcase
 
+
+
     if colours.include?(colour)
+
         puts "Your card is #{colour}"
+
         puts `clear`
+
         return colour
+
     else
+
         puts "I do not understand"
+
         getAColor
+
     end
 
+
+
 end
+
+
 
 def subtype(type)
+
     if type == "spell"
+
          puts "What attribute would you like your spell to have? \n Deal damage \n Counter a spell \n Mend damage" 
+
         subtype_spell = gets.chomp  
+
         spell_ability(subtype_spell)
+
         return subtype_spell
+
     elsif type == "creature"
+
         puts "What ability would you like your creature to have? \n Double Strike \n Lifegain \n Speed "
+
         subtype_creature = gets.chomp
+
         creature_ability(subtype_creature)
+
         return subtype_creature
+
     end     
+
 end
 
+
+
 def spell_ability(subtype)
+
     if subtype == "Deal damage"
+
         puts "You like power"
+
     end
 
+
+
     if subtype == "Counter a spell"
+
         puts "You like to protect"
+
     end
+
+
 
     if subtype == "Mend damage"
 
+
+
         puts "You like to heal"
+
     end
+
 end
+
+
 
 def creature_ability(subtype)
+
     if subtype == "Double strike"
+
         puts "You covert strength"
+
     end
+
+
 
     if subtype == "Lifegain"
+
         puts "you favour survivability"
+
     end
+
+
 
     if subtype == "Speed"
+
         puts "those that strike first, win"
+
     end
+
 end
+
+
 
 def buildACard
+
     puts `clear`
+
     puts "How much does your card cost in mana"
+
     cost = gets.chomp
 
+
+
     if cost.to_i.to_s != cost
+
         puts "That is not a number"
+
         buildACard    
+
     end
+
     
+
     
+
     
+
+
+
 
 
     puts `clear`
+
     puts "Is your card a spell or a creature"  
+
     type = gets.chomp
+
     # If type is x, use subtext method
+
     subtext(type)
+
     colour = getAColor
 
+
+
     puts "What is the name of your creation" 
+
     name = gets.chomp 
+
     puts `clear`
+
+
 
     ability = subtype(type)
+
     puts `clear`
+
     puts "Would you like to include a description?" 
+
     flavour = gets.chomp 
+
     puts `clear`
+
     puts "Congrats! #{name} is born."
 
+
+
     CSV.open("card_list.csv", "a+", {headers: true}) do |csv|
+
         csv << [cost, name, colour, type, ability , flavour]
+
     end
+
     return {cost: cost, name: name, colour: colour, type: type, ability: ability, flavour: flavour}
+
 end
 
+
+
     
+
+
 
     #edit(cost, type, colour, name, flavour)
 
 
 
+
+
+
+
 def showHeader
+
     puts `artii 'Mana Clash'`.colorize(:color => :blue)      
+
     puts `artii 'You are'`.colorize(:color => :red)  
+
     puts `artii 'the creator'`.colorize(:color => :green)
+
 end
 
 
 
 
+
+
+
+
+
 # Above this line is all my methods
+
 # This is where you now call your methods
+
 showHeader
+
+
 
 loop do
 
+
+
             puts "Welcome to Manaclash card creator and editor! What would you like to do:"
 
+
+
         
+
+
 
             puts "1. Create a new card"
 
+
+
         
+
+
 
             puts "2. Edit a card"
 
+
+
         
+
+
 
             puts "3. Remove a card"
 
+
+
         
+
+
 
             puts "4. Exit"
 
+
+
         
 
+
+
             print "Enter Option Number: "
+
+
+
+
 
 
 
@@ -167,57 +332,85 @@ loop do
 
 
 
+
+
+
+
         if input == "1"
 
+
+
              card = buildACard
+
              puts card
+
              cards << card
+
         
+
         elsif input == "2"
+
+
 
             prompt = TTY::Prompt.new
 
+
+
             puts `clear`
+
             puts   "What card would you like to edit? \n
+
                                                         "
-            puts    CSV.read("Card_list.csv", headers: true)
-            name = gets.chomp 
+
+            card_list = CSV.read("Card_list.csv", headers: true)
+
+            puts card_list
+
+            # edit = gets.chomp
+            
+            # card_list = card_list.find{|name| name == [input]}
+            # p found_item
+
+
+
+
 
 
         elsif input == "3"
 
 
 
+
+
+
+
             puts "This WILL remove your cards"
+
+
 
         elsif input ==  "4"
 
+
+
             break
 
+
+
         else 
+
+
 
           puts "anything"
 
 
+
+
+
         end
 
+
+
     
+
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
-
 
--- Markdown/development-log - day 2.md
+++ Markdown/development-log - day 2.md
@@ -1,9 +1,11 @@
 
 
-##                       Update 10:35 - Wednesday the 4th of Septemeber 
+##                       Update 11:30 - Thursday the 5th of Septemeber 
 
-I just had an ephihany. My edit feature will just need to edit strings - not my CSV file simplifying my final coding task immmensely. I am pretty sure you told me this yesterday but I interpreted him wrong, ah well. 
-
-I still have much more to do. I am about a third of the way into my documentation. I 
-
+I just spent an hour wondering if my "Edit" feature will just need to edit strings - not my CSV file - simplifying my final coding task immensely but I'm pretty sure this is wrong I will need to write straight to CSV as I initially thought. 
+ 
+Once this feature is implemented I am ready to introduce some testing tools for my application. I will use the Rubocop gem I have installed to test for style and convention. I will have to make around 10 more commits as well to make the quota.
+ 
+I still have much more documentation to do. my 'statement of purpose' and 'implementation plan' are progressing. I have to make sure I address all the language elements and concepts in my app.
+I have been taking regular screenshots of my Trello board so that task is already taken care of. My control flow chart still needs to be researched and produced as well. Finally I will need to write my help file and prepare for my presentation.
 
